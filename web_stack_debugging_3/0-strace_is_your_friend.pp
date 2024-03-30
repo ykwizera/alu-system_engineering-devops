ae97d5a9ad0700e80b529b76294e0d72f528706b
ykwizera <y.kwizera@alustudent.com>
2024-03-30 07:51:22 +0000

    using strace

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# puppet code to fix 'wp-setting.php'
+
+exec { 
+  '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
