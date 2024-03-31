0e30a833b5cf4a7348c61d3189e965195d9b71d3
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-03-31 22:57:51 +0200

    fixing apache

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
-# Puppet manifest to fix Apache 500 error
-exec { 'fix-apache-error':
-  command     => '/bin/echo "Fixing Apache error..."', # replace this with the actual fix command
-  refreshonly => true,
-  subscribe   => File['/path/to/affected/file'], # ensure this runs only when necessary
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-file { '/path/to/affected/file':
-  ensure => present, # ensure the file exists
-  # other attributes as necessary
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Define other resources as needed to ensure the fix is applied correctly
